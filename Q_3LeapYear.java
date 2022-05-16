8276f41938fef78801096bda3ca2935f2fb5a6b7
Rahul Kothawade <rahulkothawade99@gmail.com>
2022-05-16 15:58:20 +0530

    Using if_else Leap Year import scanner

---
+++ Q_3LeapYear.java
@@ -0,0 +1,20 @@
+package Day_1_Assignment;
+
+import java.util.Scanner;
+
+public class Q_3LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int year;
+		System.out.println("Enter An Year::");
+		Scanner sc= new Scanner(System.in);
+		year=sc.nextInt();
+		if(((year%4==0)&&(year%100!=0))||(year%400==0))
+		System.out.println("leap year");
+		else
+		System.out.println("not a leap year");
+	 
+}
+}
