159167a8c2bfd506b50b82dbbf5e24f0c4ca7ff1
Rahul Kothawade <rahulkothawade99@gmail.com>
2022-05-16 15:51:10 +0530

    Display Massage Output

---
+++ Q_1DisplayMessage.java
@@ -0,0 +1,23 @@
+package Day_1_Assignment;
+
+public class Q_1DisplayMessage {
+
+//1.1 write Program to display any massage :
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a= 10;
+		int b= 20;
+		int c= (a+b);
+		
+		System.out.println("a and b is equals to c=");
+		System.out.println(c);
+	}
+}
+
+
+
+
+    
+	
+
